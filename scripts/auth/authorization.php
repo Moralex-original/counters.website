958b176c8355914b67b5a5838d9fc40ea6796f8c
Moralex-original <moralex_original@outlook.com>
2023-02-19 19:47:44 +0800

    a

--- scripts/auth/authorization.php
+++ scripts/auth/authorization.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-      <title>Chapter-1</title> <!-- Заголовок страницы в браузере (не более 60 символов) -->
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
-      <link rel="stylesheet" href="../../css/style_reg.css"> <!-- Подключаение CSS таблицу стилей -->
-      <!-- Настройка окна просмотра -->
-      <meta name="author" content="Морщинин Александр"> <!-- Указание автора -->
-      <meta name="keywords" content="КБЖУ, счетчик калорий, подсчет финансов"> <!-- Cписок ключевых слов -->
-      <meta name="description" content="Подсчет КБЖУ, и прочих прелестей жизни"> <!-- Описание содержимого страницы -->
-   </head>
+
+<head>
+   <title>Chapter-1</title> <!-- Заголовок страницы в браузере (не более 60 символов) -->
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+   <link rel="stylesheet" href="../../css/style_reg.css"> <!-- Подключаение CSS таблицу стилей -->
+   <!-- Настройка окна просмотра -->
+   <meta name="author" content="Морщинин Александр"> <!-- Указание автора -->
+   <meta name="keywords" content="КБЖУ, счетчик калорий, подсчет финансов"> <!-- Cписок ключевых слов -->
+   <meta name="description" content="Подсчет КБЖУ, и прочих прелестей жизни"> <!-- Описание содержимого страницы -->
+</head>
+
 <body>
 
    <!-- Регистрация и авторизация-->
    <div id=reg_inside>
       <form id=reg_win action="" method="">
-      <label>Имя</label>
-      <input type="text" placeholder="Введите имя">
-      <label>Пароль</label>
-      <input type="password" placeholder="Введите пароль">
-      <button>Войти</button>
-      <button href="../reg/registrtion.php">Регистрация</button>
+         <label>Имя</label>
+         <input type="text" placeholder="Введите имя">
+         <label>Пароль</label>
+         <input type="password" placeholder="Введите пароль">
+         <button>Войти</button>
+         <form action="../reg/registrtion.php" target="_blank"><button>Регистрация</button>
+         </form>
       </form>
-   </div>   
-   
+   </div>
+
 </body>
+
 </html>
