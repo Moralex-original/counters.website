3b5c64b5a1620530cf7bebc0e0c4651330d2d5db
Moralex-original <moralex_original@outlook.com>
2023-02-19 09:23:02 +0800

    font

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 <html lang="en">
 
 <head>
+   <title>Counters_and_Calculater</title> <!-- Заголовок страницы в браузере (не более 60 символов) -->
    <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+   <link rel="stylesheet" href="style.css"> <!-- Подключаение CSS таблицу стилей -->
+   <!-- Настройка окна просмотра -->
+   <meta name="author" content="Морщинин Александр"> <!-- Указание автора -->
+   <meta name="keywords" content="КБЖУ, счетчик калорий, подсчет финансов"> <!-- Cписок ключевых слов -->
+   <meta name="description" content="Подсчет КБЖУ, и прочих прелестей жизни"> <!-- Описание содержимого страницы -->
    <title>Титул</title>
 </head>
 
