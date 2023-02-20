8194f4d41d7b01e50e8ce0c6209690c3dbb25e8c
Moralex-Original <moralex_original@outlook.com>
2023-02-20 12:08:53 +0800

    msg

--- scripts/reg/registration.php
+++ scripts/reg/registration.php
@@ -1,20 +1,22 @@
 <?php
-   //Запуск сессии
-   session_start();
+//Запуск сессии
+session_start();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-      <title>Registration</title> <!-- Заголовок страницы в браузере (не более 60 символов) -->
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
-      <link rel="stylesheet" href="../../css/style_reg.css"> <!-- Подключаение CSS таблицу стилей -->
-      <!-- Настройка окна просмотра -->
-      <meta name="author" content="Морщинин Александр"> <!-- Указание автора -->
-      <meta name="keywords" content="КБЖУ, счетчик калорий, подсчет финансов"> <!-- Cписок ключевых слов -->
-      <meta name="description" content="Подсчет КБЖУ, и прочих прелестей жизни"> <!-- Описание содержимого страницы -->
-   </head>
+
+<head>
+   <title>Registration</title> <!-- Заголовок страницы в браузере (не более 60 символов) -->
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+   <link rel="stylesheet" href="../../css/style_reg.css"> <!-- Подключаение CSS таблицу стилей -->
+   <!-- Настройка окна просмотра -->
+   <meta name="author" content="Морщинин Александр"> <!-- Указание автора -->
+   <meta name="keywords" content="КБЖУ, счетчик калорий, подсчет финансов"> <!-- Cписок ключевых слов -->
+   <meta name="description" content="Подсчет КБЖУ, и прочих прелестей жизни"> <!-- Описание содержимого страницы -->
+</head>
+
 <body>
 
    <!-- Регистрация -->
@@ -28,20 +30,27 @@
          <input type="password" name="password" placeholder="Введите пароль">
          <label>Подтвердите пароль</label>
          <input type="password" name="password_confirm" placeholder="Подтвердите пароль">
-         <button>Войти</button>         
-      </form>
-      <form action="../../scripts/auth/authorization.php" target="_self">
-            <button>Авторизация</button>
-      </form>
-         
+         <button>Войти</button>
          <?php
-         //Окно с выводом о несовпадении паролей
          if ($_SESSION['message']) {
             echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
          }
          unset($_SESSION['message']);
          ?>
+      </form>
+      <form action="../../scripts/auth/authorization.php" target="_self">
+         <button>Авторизация</button>
+      </form>
+
+      <?php
+      //Окно с выводом о несовпадении паролей
+      if ($_SESSION['message']) {
+         echo '<p class="msg"> ' . $_SESSION['message'] . ' </p>';
+      }
+      unset($_SESSION['message']);
+      ?>
    </div>
 
 </body>
+
 </html>
